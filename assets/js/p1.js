4a31727ec126e5f10caa99628379b4883c02b104
Morgan <morgan.smith118@gmail.com>
2021-02-03 17:11:12 -0700

    Update p1.js

--- assets/js/p1.js
+++ assets/js/p1.js
@@ -157,39 +157,13 @@ const authorizationUrl =
     //Cookies.set('name', idPayload.given_name);
   }
 
-  function setUserValues(userJson) {
-    console.log("setuserValues was called");
-    console.log(userJson);
-    let uuid = Cookies.get('uuid');
-    //let streetAddress = userJson.address.streetAddress + " " + userJson.address.locality + ", " + userJson.address.region + " " + userJson.address.postalCode;
-    if (Cookies.get("accessToken")) {
-      if(userJson.name){
-        if(userJson.name.given){
-          console.log("givenname if was passes")
-          document.getElementById("user").value = 'Hello ' + userJson.name.given + "!";
-          document.getElementById("fname").value = userJson.name.given;
-        }
-        if(userJson.name.family){
-        document.getElementById("lname").value = userJson.name.family;
-        }
-      }
-      document.getElementById("email").value = userJson.email;
-      document.getElementById("username").value = userJson.username;
-      //document.getElementById("address").innerHTML=streetAddress;
-    } else {
-      document.getElementById("username").innerHTML = 'Welcome Guest';
-    }
 
-    //let idPayload = parseJwt(idToken);
-  }
-
-
-
-
-
-
-
-
+//----initate logon ---- //
+function initiateLogon(){
+  let method = 'POST';
+   let url = authorizationUrl;
+   exJax(method, url, nextStep);
+}
 
 
 
