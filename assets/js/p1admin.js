a0cf3418ecffd93d9b760308febb6a752f5a61a3
Morgan <morgan.smith118@gmail.com>
2021-02-11 13:50:30 -0700

    Update p1admin.js

--- assets/js/p1admin.js
+++ assets/js/p1admin.js
@@ -1,19 +1,11 @@
-function adminGetUser(type){
+function adminGetUser(){
     //{{apiPath}}/environments/{{envID}}/users/?filter=username%20eq%20%22lsmith%22
     console.log('adminGetUser called');
     let method = "GET";
-    // let user = document.getElementById('PassID').value;
-    let url;
-    let at = "Bearer " + Cookies.get("accessToken");
-    if(type == "email"){
-      console.log("we made it in the if statement");
-      let value = document.getElementById('email').value;
-      url = apiUrl + "/environments/" + environmentID + "/users/?filter=email%20eq%20%22" + value + "%22";
-    }
-    // if(type == "passID"){
-    //   let url = apiUrl + "/environments/" + environmentID + "/users/?filter=username%20eq%20%22" + user + "%22";
-    // }
-    
+    let user = document.getElementById('username').value;
+    console.log(user);
+    let at = "Bearer " + Cookies.get("workerAT");
+    let url = apiUrl + "/environments/" + environmentId + "/users/?filter=username%20eq%20%22" + user + "%22";
     console.log('ajax (' + url + ')');
     console.log('at =' + at);
     console.log("make ajax call");
@@ -28,7 +20,7 @@ function adminGetUser(type){
       console.log('response '+response);
       adminSetUserValues(response);
     });
-    console.log("adminGetUser completed");
+    console.log("adminGetUser completed")
   }
 
 
@@ -43,14 +35,37 @@ function adminGetUser(type){
       document.getElementById("lname").value = userJson._embedded.users[0].name.family;
       document.getElementById("email").value = userJson._embedded.users[0].email;
       document.getElementById("username").value = userJson._embedded.users[0].username;
-
-      document.getElementById("usernameFill").innerHTML = 'Username: ' + userJson._embedded.users[0].username;
-      document.getElementById("fnameFill").innerHTML = userJson._embedded.users[0].name.given;
-      document.getElementById("lnameFill").innerHTML = userJson._embedded.users[0].name.family;
-      document.getElementById("emailFill").innerHTML = userJson._embedded.users[0].email;
-      document.getElementById("usernameFill").innerHTML = userJson._embedded.users[0].username;
       //document.getElementById("address").innerHTML=streetAddress;
     } else {
       document.getElementById("username").innerHTML = 'Welcome Guest';
     }
   }
+
+  function getWorkerAccessToken() {
+    var settings = {
+      "url": "https://auth.pingone.com/ca3ad373-df71-4eb5-a3b5-76439336e1d6/as/token",
+      "method": "POST",
+      "timeout": 0,
+      "headers": {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      "data": {
+        "grant_type": "client_credentials",
+        "client_id": "cedd8115-38d5-49f6-8bd8-043505fd83c6",
+        "client_secret": "EAarQcJAyAsS2QZN46MSrQD_nUHUK9~b2liHYlULE3jKne1EPIFwGG3Jayo6upBQ"
+      }
+    };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    setCookies(response);
+  });
+}
+
+function setCookies(data){
+  console.log("setcookie Called");
+  console.log(data.access_token);
+  let at = data.access_token;
+  console.log('at is: ' + at);
+  Cookies.set('workerAT', at, { sameSite: 'strict' });
+}
