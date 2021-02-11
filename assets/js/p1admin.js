3542c8403eb916ef4f667761b616b49a79de3651
Morgan <morgan.smith118@gmail.com>
2021-02-11 14:28:57 -0700

    Update p1admin.js

--- assets/js/p1admin.js
+++ assets/js/p1admin.js
@@ -38,11 +38,11 @@ function adminGetUser(type){
     Cookies.set('currentUser', userJson._embedded.users[0].id);
     if (Cookies.get("accessToken")) {
       document.getElementById("usernameFill").value = 'Hello ' + userJson._embedded.users[0].username + "!";
-      if(userJson._embedded.users[0].contains('name.given'))
+      if ('name.given' in userJson._embedded.user[0])
       {
          document.getElementById("fnameFill").value = userJson._embedded.users[0].name.given;
       }
-      if(userJson_embedded.users[0].contains('name.family')){
+      if(userJson_embedded.users[0]['name.family']){
       document.getElementById("lnameFill").value = userJson._embedded.users[0].name.family;
       }
       document.getElementById("emailFill").value = userJson._embedded.users[0].email;
